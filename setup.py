ec05943b49a6f71b185653ed363cecfb3dba4f94
Jan Funke <funke@ini.ch>
2016-05-29 15:28:50 -0400

    bumped package version to 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='cremi',
-    version='0.2',
+    version='0.3',
     description='Python Package for the CREMI Challenge',
     author='Jan Funke',
     author_email='jfunke@iri.upc.edu',
