8a6394d85b7fda96a23707cdc5d6c0a20441627b
Jan Funke <funke@ini.ch>
2016-05-29 10:16:40 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cremi',
+    version='0.1',
+    description='Python Package for the CREMI Challenge',
+    author='Jan Funke',
+    author_email='jfunke@iri.upc.edu',
+    url='http://github.com/funkey/cremi_python',
+    packages=['cremi', 'cremi.io'],
+)
