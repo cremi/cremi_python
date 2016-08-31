ab338c7f97a56d46618a11fa28f44790eb608831
Jan Funke <jfunke@iri.upc.edu>
2016-08-31 12:16:41 -0400

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='cremi',
-    version='0.6',
+    version='0.7',
     description='Python Package for the CREMI Challenge',
     author='Jan Funke',
     author_email='jfunke@iri.upc.edu',
